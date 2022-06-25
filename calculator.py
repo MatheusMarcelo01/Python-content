e0a7a5af06e2d983e6960ad5dd739050dd724982
Matheus Marcelo <107058893+MatthDev01@users.noreply.github.com>
2022-06-25 13:13:05 -0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+from tkinter import Tk, Label, RAISED
+raiz =  Tk()
+labels   = [['MC', 'M+',   'M-',   'MR'],
+            ['C','\u221a','x\u00b2','+'],
+            ['7',    '8',     '9', '-' ],
+            ['4',    '5',     '6',  '-'],
+            ['1',    '2',     '3',  '*'],
+            ['0',    '.',     '+-', '#']]
+
+for r in range(6):
+    for c in range(4):
+        b= buttons
