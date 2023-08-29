66c0dcce13d256c9ca0a2039af79bbe3f9dbcb8a
Matheus Marcelo <matheusmpmenezes@protonmail.com>
2023-08-28 22:07:17 -0300

    Update window.py

--- window.py
+++ window.py
@@ -1,4 +1,10 @@
+#Teste com tkinter
 from tkinter import Tk,Label, PhotoImage, TOP, BOTTOM
+''' tkinter que serão usadas para criar a interface gráfica (GUI).
+Tk é a classe principal que representa a janela da GUI, 
+Label é usado para exibir texto ou imagens, 
+PhotoImage é usado para exibir imagens
+TOP e BOTTOM são constantes que representam a posição dos widgets. '''
 
 root = Tk()
 
